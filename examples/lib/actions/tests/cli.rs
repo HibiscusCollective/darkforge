62b1328e1aa297b65daef57e865e428f18399008
Pierre Fouilloux <7494211+pfouilloux@users.noreply.github.com>
2025-05-04 23:07:41 +0800

    feat(examples/lib/actions): implement the help flag

--- examples/lib/actions/tests/cli.rs
+++ examples/lib/actions/tests/cli.rs
@@ -1,10 +1,33 @@
-use assert_cmd::{Command, cargo_bin};
+use std::{path::Path, sync::LazyLock};
 
-#[test]
-fn test_prints_version_given_version_flag_is_set() {
-    Command::new(cargo_bin!("example-act"))
-        .arg("--version")
+use assert_cmd::{Command, cargo_bin};
+use indoc::indoc;
+use rstest::rstest;
+
+static BINARY: LazyLock<&Path> = LazyLock::new(|| cargo_bin!("forge"));
+
+#[rstest]
+#[case::short("-V")]
+#[case::full("--version")]
+fn test_prints_version_given_version_flag_is_set(#[case] flag: &str) {
+    Command::new(BINARY.clone())
+        .arg(flag)
         .assert()
         .success()
         .stdout("Forge Actions Example 0.1.0\n");
+}
+
+#[rstest]
+#[case::short("-h")]
+#[case::full("--help")]
+fn test_prints_help_given_help_flag_is_set(#[case] flag: &str) {
+    Command::new(BINARY.clone()).arg(flag).assert().success().stdout(indoc!(
+        "
+            Usage: forge
+
+            Options:
+              -h, --help     Print help
+              -V, --version  Print version
+        "
+    ));
 }
