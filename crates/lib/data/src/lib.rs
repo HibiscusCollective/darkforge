e54d35271750ed37a760e081175ad9e902d29fa7
Pierre Fouilloux <7494211+pfouilloux@users.noreply.github.com>
2025-05-14 18:54:31 +0800

    docs(data/uuid): document the rest of the data crate

--- crates/lib/data/src/lib.rs
+++ crates/lib/data/src/lib.rs
@@ -10,9 +10,16 @@
  * You should have received a copy of the GNU Affero General Public License along with this program.
  * If not, see https://www.gnu.org/licenses/.
  */
+#![deny(missing_docs)]
 
-extern crate core;
+//! Data module for the Dark Forge library.
+//!
+//! This module provides data storage for games, with flexible structures to create different settings and support multiple languages.
+//! By default, the data is stored in two tiers:
+//!     - The first tier is read-only static data, usually loaded on startup and during transition. This store is read-optimised.
+//!     - The second tier is dynamic static data, typically game state that we may want to serialize into a save file. Mostly held in memory, but disk backed if extra storage is required.
 
+/// Module for data storage.
 pub mod store;
 mod uuid;
 
@@ -22,15 +29,23 @@ use thiserror::Error;
 
 use crate::uuid::Uuid;
 
+/// Result type for operations in the data crate.
 pub type Result<T> = std::result::Result<T, DataError>;
+
+/// Pointer to a component, wrapped in an `Arc` and `RwLock`.
 pub type ComponentPtr<'a> = Arc<RwLock<dyn Component + 'a>>;
+
+/// Entity identifier type, implemented as a UUID.
 pub type Entity = Uuid;
 
+/// Trait for data components.
 pub trait Component {}
 
+/// Error type for operations in the data crate.
 #[derive(Error, Debug)]
 pub enum DataError {}
 
+/// Creates a new component pointer from a component.
 pub fn component_ptr<'a>(component: impl Component + 'a) -> ComponentPtr<'a> {
     Arc::new(RwLock::new(component))
 }
